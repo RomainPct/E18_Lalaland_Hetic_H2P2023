882ec10b4fd59b7c7001c1ab4eeb8184d5a36a38
Nicolas Decreton <47737539+NicolasDecreton@users.noreply.github.com>
2019-10-09 22:10:25 +0200

    push menu

--- pages/elements/header.php
+++ pages/elements/header.php
@@ -5,19 +5,19 @@
         <h2>MENU</h2>
         <ul>
             <li>
-                 <a href="/Accueil"><p>Accueil</p></a>
+                 <a href="/Accueil">Accueil</a>
             </li>
             <li>
-                 <a href="/Synopsis"><p>Synopsis</p></a>
+                 <a href="/Synopsis">Synopsis</a>
             </li>
             <li>
-                <a href="/Casting"><p>Casting</p></a>
+                <a href="/Casting">Casting</a>
             </li>
             <li>
-                <a href="/Galerie"><p>Galerie</p></a>
+                <a href="/Galerie">Galerie</a>
             </li>
             <li>
-                <a href="/Musique"><p>Musiques</p></a>
+                <a href="/Musique">Musiques</a>
             </li>
         </ul>
     </div>
