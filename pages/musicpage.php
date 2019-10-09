785546458ba04a88fe7af2568c01db15752c8845
Nicolas Decreton <47737539+NicolasDecreton@users.noreply.github.com>
2019-10-09 12:58:14 +0200

    push music

--- pages/musicpage.php
+++ pages/musicpage.php
@@ -3,12 +3,16 @@
 </section>
 
 <section id="musicPageContainer">
-    <div id="musicPageLeftBox">
-    <iframe src="https://open.spotify.com/embed/album/3GU8BzFEAdFSRjc8jZkL3S" width="620" height="340" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-        <div id="iconsBox">
+    <div id="musicPageContainer__leftBox">
+         <iframe src="https://open.spotify.com/embed/album/3GU8BzFEAdFSRjc8jZkL3S" width="620" height="340" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        <div id="musicPageContainer__iconsBox">
+            <a href="https://music.apple.com/fr/album/la-la-land-original-motion-picture-soundtrack/1440863506"><img src="../assets/images/logoitunes.png" alt="itunes"></a>
+            <a href="https://music.amazon.fr/albums/B01N74N5LW?ref=dm_sh_de4c-7a80-dmcp-97ad-1e766&musicTerritory=FR&marketplaceId=A13V1IB3VIYZZH"><img src="../assets/images/logoamazon.png" alt="amazon music"></a>
+            <a href="https://music.amazon.fr/albums/B01N74N5LW?ref=dm_sh_de4c-7a80-dmcp-97ad-1e766&musicTerritory=FR&marketplaceId=A13V1IB3VIYZZH"><img src="../assets/images/logogoogleplay.png" alt="amazon music"></a>
+
         </div>
     </div>
-    <div id="musicPageRightBox">
+    <div id="musicPageContainer__rightBox">
         <img src="../assets/images/albumcover.jpg" alt="album">
     </div>
 </section>
