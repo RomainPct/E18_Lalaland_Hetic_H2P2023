6dafb481c542e02a51097fd37a2951f9e2c71c6b
augustinmbt <augustinmbath@gmail.com>
2019-10-09 17:29:59 +0200

    home

--- pages/accueil.php
+++ pages/accueil.php
@@ -3,20 +3,20 @@ $nextPageLink = "/synopsis";
 $nextPageName = "Le synopsis";
 ?>
 <section id="home">
-    <img src="assets/images/laladance.jpg" class="hero_background" alt="">
+    
+    <div class="hero">
     <span class="title">La La Land</span>
     <a id="js_launchExperience" class="btn hero_btn" href="#experience">Lancer l'expérience</a>
-   <div class="awards_content">
-    <div class="awards">
-        <img src="assets/images/awardvenice.png" class="toronto" alt="">
-        <img src="assets/images/awarddamien.png" class="damienc" alt="">
-        <img src="assets/images/awardsong.png" class="song" alt="">
-        <img src="assets/images/awardproduction.png" class="production" alt="">
-        <img src="assets/images/awardvenice.png" class="venice" alt="">
-
     </div>
-    </div>
+        <div class="awards">
+            <img src="assets/images/awardvenice.png" class="toronto" alt="">
+            <img src="assets/images/awarddamien.png" class="damienc" alt="">
+            <img src="assets/images/awardsong.png" class="song" alt="">
+            <img src="assets/images/awardproduction.png" class="production" alt="">
+            <img src="assets/images/awardvenice.png" class="venice" alt="">
+        </div>
 </section>
+
 <section id="js_experience" class="experience">
     <div class="experience__navigator" id="js_experienceNavigator">
         <div>
