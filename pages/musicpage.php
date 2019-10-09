3e27c4faa6234ae2a8482710573dc9e30d899ea2
Nicolas Decreton <47737539+NicolasDecreton@users.noreply.github.com>
2019-10-09 14:35:41 +0200

    commit php

--- pages/musicpage.php
+++ pages/musicpage.php
@@ -1,10 +1,16 @@
+
+<?php
+$nextPageLink = "http://www.allocine.fr/";
+$nextPageName = "Réserver votre place";
+?>
+
 <section id="musicPageTitle">
-    <h3>Bande Originale</h3>
+    <h2>Bande Originale</h2>
 </section>
 
 <section id="musicPageContainer">
     <div id="musicPageContainer__leftBox">
-         <iframe src="https://open.spotify.com/embed/album/3GU8BzFEAdFSRjc8jZkL3S" width="620" height="340" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+         <iframe src="https://open.spotify.com/embed/album/3GU8BzFEAdFSRjc8jZkL3S" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
         <div id="musicPageContainer__iconsBox">
             <a href="https://music.apple.com/fr/album/la-la-land-original-motion-picture-soundtrack/1440863506"><img src="../assets/images/logoitunes.png" alt="itunes"></a>
             <a href="https://music.amazon.fr/albums/B01N74N5LW?ref=dm_sh_de4c-7a80-dmcp-97ad-1e766&musicTerritory=FR&marketplaceId=A13V1IB3VIYZZH"><img src="../assets/images/logoamazon.png" alt="amazon music"></a>
@@ -13,6 +19,9 @@
         </div>
     </div>
     <div id="musicPageContainer__rightBox">
-        <img src="../assets/images/albumcover.jpg" alt="album">
+        <img id="albumCover" src="../assets/images/albumcover.jpg" alt="album">
+        <div>
+        <a href=""><img src="../assets/images/logovinyl.png" alt="album"></a>
+        </div>
     </div>
 </section>
