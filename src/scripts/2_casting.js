3a1a8c20a78156c84c1e044ee29d4a0c84f810f6
Tina Elh <tinaelha@gmail.com>
2019-10-09 18:57:51 +0200

    détecter le mouvement du scroll et position JS page casting

--- src/scripts/2_casting.js
+++ src/scripts/2_casting.js
@@ -4,7 +4,10 @@ let imagesCasting = document.querySelectorAll(".characterContainer__img")
 let descriptionActors = document.querySelectorAll(".characterContainer__description")
 
 // détecter l'événement de scroll
+window.onscroll = function (e){
+    console.log("je suis en train de scroll débile")
     // Récupérer la position du scroll sur Y
+    let scroll = window.scrollY
     // Pour chaque image de l'effet
         // Récupérer la position de l'image sur l'écran de l'user
         // Si elle est visible
@@ -12,4 +15,6 @@ let descriptionActors = document.querySelectorAll(".characterContainer__descript
     // Pour chaque __description
         // Récupérer la position de la __description sur l'écran de l'user
         // Si elle est visible
-            // Appliquer un transform selon la valeur du scroll        
+            // Appliquer un transform selon la valeur du scroll  
+
+}      
