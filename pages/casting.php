da858a1bcb053b5ad09d91b306b9e99372cfa504
Tina Elh <tinaelha@gmail.com>
2019-10-09 11:59:54 +0200

    debut page casting HTML SCSS

---
+++ pages/casting.php
@@ -0,0 +1,20 @@
+<section class="containerRyanGosling">
+    <div class="containerRyanGosling__ImageRyan">
+        <img src="" alt="Ryan Gosling">
+    </div>
+    <div class="containerRyanGosling__descriptionRyan">
+        <h2>Ryan Gosling</h2>
+        <p>Dans le rôle de Sebastien Wilder </p>
+        <p>Acteur et producteur de cinéma, a dès son plus jeune âge exercé la danse. Il a tourné dans le célèbre drame romantique N’oublie jamais (The Notebook).</p>
+    </div>
+</section>
+<section class="containerEmmaStone">
+    <div class="containerEmmaStone__ImageEmma">
+        <img src="" alt="Emma Stone">
+    </div>
+    <div class="containerEmmaStone__descriptionEmma">
+        <h2>Emma Stone</h2>
+        <p>Dans le rôle de Mia Dolan</p>
+        <p>L’actrice américaine s’est imposée dans le monde du cinéma grâce à son expérience dans des classiques tels que La Couleur des sentiments et The Amazing Spider-man.</p>
+    </div>
+</section>
