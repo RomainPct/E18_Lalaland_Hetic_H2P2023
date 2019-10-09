c09f604c8dd7270f8620d1b200036718271247a4
Romain Penchenat <romain.penchenat@gmail.com>
2019-10-09 16:56:43 +0200

    Page synopsis

--- pages/synopsis.php
+++ pages/synopsis.php
@@ -2,3 +2,9 @@
 $nextPageLink = "/casting";
 $nextPageName = "Le casting";
 ?>
+<section class="synopsis">
+    <h2>Synopsis</h2>
+    <p>
+        La La Land raconte l’histoire de Mia, une actrice en herbe interpretée par Emma Stone, et celle de Sebastian, un musicien dévoué au jazz joué par Ryan Gosling, qui ne raccordent pas les deux bouts dans la fameuse ville aux milles étoiles si connue pour détruire les rêves et espoirs de chacun. Suivez dans ce Los Angeles moderne cette comédie musicale qui exploite les joies et souffrances de la poursuite de ses rêves !
+    </p>
+</section>
