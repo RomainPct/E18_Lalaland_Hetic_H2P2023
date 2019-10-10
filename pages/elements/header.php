706726a15f81ab118c71b88e57e1726db13d61a4
Romain Penchenat <romain.penchenat@gmail.com>
2019-10-10 09:40:08 +0200

    Finitions menu

--- pages/elements/header.php
+++ pages/elements/header.php
@@ -5,19 +5,19 @@
         <h2>MENU</h2>
         <ul>
             <li>
-                 <a href="/Accueil">Accueil</a>
+                 <a href="/accueil">Accueil</a>
             </li>
             <li>
-                 <a href="/Synopsis">Synopsis</a>
+                 <a href="/synopsis">Synopsis</a>
             </li>
             <li>
-                <a href="/Casting">Casting</a>
+                <a href="/casting">Casting</a>
             </li>
             <li>
-                <a href="/Galerie">Galerie</a>
+                <a href="/galerie">Galerie</a>
             </li>
             <li>
-                <a href="/Musique">Musiques</a>
+                <a href="/musique">Musiques</a>
             </li>
         </ul>
     </div>
