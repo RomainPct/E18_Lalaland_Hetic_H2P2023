1215190fcfc0890c876359639817ca92200fd8d7
Romain Penchenat <romain.penchenat@gmail.com>
2019-10-10 15:38:14 +0200

    Lazy loading and improvments

--- pages/musique.php
+++ pages/musique.php
@@ -1,4 +1,3 @@
-
 <?php
 $nextPageLink = "http://www.allocine.fr/film/fichefilm_gen_cfilm=229490.html";
 $nextPageName = "La La Land";
@@ -9,20 +8,28 @@ $nextPageName = "La La Land";
     <h2>Bande Originale</h2>
     <div class="musicPageContainer__Sub">
         <div class="musicPageContainer__Sub__leftBox">
-             <iframe src="https://open.spotify.com/embed/album/3GU8BzFEAdFSRjc8jZkL3S" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-         <div class="musicPageContainer__Sub__leftBox__iconsBox">
-                <a href="https://music.apple.com/fr/album/la-la-land-original-motion-picture-soundtrack/1440863506"><img src="../assets/images/imagesMusique/logoitunes.png" alt="itunes"></a>
-             <a href="https://music.amazon.fr/albums/B01N74N5LW?ref=dm_sh_de4c-7a80-dmcp-97ad-1e766&musicTerritory=FR&marketplaceId=A13V1IB3VIYZZH"><img src="../assets/images/imagesMusique/logoamazon.png" alt="amazon music"></a>
-             <a href="https://music.amazon.fr/albums/B01N74N5LW?ref=dm_sh_de4c-7a80-dmcp-97ad-1e766&musicTerritory=FR&marketplaceId=A13V1IB3VIYZZH"><img src="../assets/images/imagesMusique/logogoogleplay.png" alt="amazon music"></a>
-
+            <iframe src="https://open.spotify.com/embed/album/3GU8BzFEAdFSRjc8jZkL3S" frameborder="0"
+                allowtransparency="true" allow="encrypted-media"></iframe>
+            <div class="musicPageContainer__Sub__leftBox__iconsBox">
+                <a href="https://music.apple.com/fr/album/la-la-land-original-motion-picture-soundtrack/1440863506">
+                    <img src="/assets/images/imagesMusique/logoitunes.png" alt="Ecouter La La Land sur iTunes">
+                </a>
+                <a href="https://music.amazon.fr/albums/B01N74N5LW?ref=dm_sh_de4c-7a80-dmcp-97ad-1e766&musicTerritory=FR&marketplaceId=A13V1IB3VIYZZH">
+                    <img src="/assets/images/imagesMusique/logoamazon.png" alt="Ecouter La La Land sur Apple Music">
+                </a>
+                <a href="https://play.google.com/store/music/artist/La_La_Land_Cast?id=A7jxpydbjesvld3bo5szuvixhcu&hl=en_US">
+                    <img src="/assets/images/imagesMusique/logogoogleplay.png" alt="Ecouter La La Land sur Google Music">
+                </a>
             </div>
-     </div>
+        </div>
         <div class="musicPageContainer__Sub__rightBox">
-             <img class="albumCover" src="../assets/images/imagesMusique/albumcover.jpg" alt="album">
-             <div class="musicPageContainer__Sub__rightBox__iconsBox">
-             <a href=""><img src="../assets/images/imagesMusique/logovinyl.png" alt="album"></a>
-             </div>
+            <img class="albumCover" src="../assets/images/imagesMusique/albumcover.jpg" alt="album">
+            <div class="musicPageContainer__Sub__rightBox__iconsBox">
+                <a href="https://www.fnac.com/a10311259/Bande-originale-de-film-La-La-Land-Vinyle-album?esl-k=sem-google%7cng%7cc258108183051%7cm%7ckpla297888842381%7cp%7ct%7cdc%7ca56985827681%7cg1263923012&gclid=EAIaIQobChMI-KCb4OeR5QIVFp7VCh0soATBEAQYASABEgIFwPD_BwE&gclsrc=aw.ds&oref=00000000-0000-0000-0000-000000000000&storecode=4019">
+                    <img src="/assets/images/imagesMusique/logovinyl.png" alt="album">
+                </a>
+            </div>
         </div>
     </div>
-    
+
 </section>
