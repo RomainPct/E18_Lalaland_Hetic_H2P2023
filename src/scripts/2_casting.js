2bdf21509fdfd1905912764f3635568339a06322
Tina Elh <tinaelha@gmail.com>
2019-10-10 09:12:43 +0200

    function setcasting JS

--- src/scripts/2_casting.js
+++ src/scripts/2_casting.js
@@ -1,20 +1,27 @@
 // SelectAll les images qui doivent faire parti de l'effet
 let imagesCasting = document.querySelectorAll(".characterContainer__img")
 // SelectAll les __description qui doivent faire parti de l'effet
-let descriptionActors = document.querySelectorAll(".characterContainer__description")
+let descriptionActors = document.querySelectorAll(".characterContainer__description")    
 
-// détecter l'événement de scroll
-window.onscroll = function (e){
-    console.log("je suis en train de scroll débile")
-    // Récupérer la position du scroll sur Y
-    let scroll = window.scrollY
-    // Pour chaque image de l'effet
-        // Récupérer la position de l'image sur l'écran de l'user
-        // Si elle est visible
-            // Appliquer un transform selon la valeur du scroll
-    // Pour chaque __description
-        // Récupérer la position de la __description sur l'écran de l'user
-        // Si elle est visible
-            // Appliquer un transform selon la valeur du scroll  
+function setCasting() {
+    // détecter l'événement de scroll
+    window.onscroll = function (e){
+        console.log("je suis en train de scroll débile")
+        // Récupérer la position du scroll sur Y
+        let scroll = window.scrollY
+        // Pour chaque image de l'effet
+        //for(imagesCasting)
+            // Récupérer la position de l'image sur l'écran de l'user
+            // Si elle est visible
+                // Appliquer un transform selon la valeur du scroll
+        // Pour chaque __description
+            // Récupérer la position de la __description sur l'écran de l'user
+            // Si elle est visible
+                // Appliquer un transform selon la valeur du scroll  
 
-}      
+    }  
+}
+
+if (window.location.pathname == "/casting") {
+    setCasting()
+}
